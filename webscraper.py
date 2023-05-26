78963665c788175c14ea93d2dc666bcc90b4f524
ZS <46884813+MoulikRT@users.noreply.github.com>
2023-05-26 16:03:03 +0530

    Add files via upload

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def some(i,dir,url):
-    with open(dir+str(i)+'.html','w') as f:
-        c = requests.get(url)
-        f.write(c.content)
+    
         
-url = ""
-dir= ""
+url = "https://en.wikipedia.org/wiki/Fermat%27s_Last_Theorem"
 f = requests.get(url)
 bsobj = bs(f.content, 'html.parser')
 
@@ -15,10 +11,14 @@ unique_links = []
 
 for link in bsobj.find_all('a'):
     href = link.get('href')
-    if 'www' not in str(href): href = "https://en.wikipedia.org" + str(href)
-    if 'https://en.wikipedia.org/wiki/' in href and '.svg' not in href and 'jpg' not in href: unique_links.append(href)
-    if len(unique_links) >= 50:
+    if 'www' not in str(href): href = "en.wikipedia.org" + str(href)
+    if 'en.wikipedia.org/wiki/' in href and '.svg' not in href and ':' not in href and 'jpg' not in href: unique_links.append(href)
+    if len(unique_links) >= 500:
         break
-i= 0 
-for link in unique_links:
-    some(i,dir,link)
+
+with open('output.txt','w') as f:
+        i= 0 
+        for link in unique_links:
+            print(i, "/", len(unique_links)-1)
+            f.write(str(link)+"\n")
+            i+=1
